a5828934b97e5b2a4fbdc79a0ecf4049042202e4
José L. Ortiz <jlortiz@uc.cl>
2025-06-05 15:23:06 -0400

    Update __init__.py
    Removed misleading constants from code

--- fsr_lister/__init__.py
+++ fsr_lister/__init__.py
@@ -5,8 +5,8 @@ app = Flask(__name__, instance_relative_config=True)
 app.config.update(
     dict(
         TOKEN="define in instance/application.cfg",
-        API_CALL_URL="https://asa.alma.cl/webslt/service/api/entries",
-        SECRET_KEY="N563gGabaQlav1gsKn",
+        API_CALL_URL="define in instance/application.cfg",
+        SECRET_KEY="define in instance/application.cfg",
     )
 )
 app.config.from_pyfile("application.cfg", silent=True)
